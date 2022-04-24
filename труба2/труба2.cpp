938b375b7f83956a76351275b8769c532c023e8f
Alexandr Trubin <trubin1@yandex.ru>
2022-04-24 18:18:12 +0300

    изменение 18:16

--- труба2/труба2.cpp
+++ труба2/труба2.cpp
@@ -8,22 +8,21 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int a ,b;
-	cin >> a >> b;
-	float c;
-	c = a + b;
-	cout << c << endl;
-	c = a - b;
-	cout << c << endl;
-	c = a * b;
-	cout << c << endl;
-	c = (float)a / b;
-	cout << c << endl;
-	a += b;//a=a+b;
-	a += 1;
-		a++;//инкримент;
-		++a;
-		a--;//декримент;
-		--a;
+
+	cout << "Вычисление стоимости поокупки\n";
+	cout << "Введите исходные данные:\n";
+	cout << "Цена тетради(руб)->";
+	float price_notebook, price_pencil, summa;
+	int cout_notebook, cout_pencil;
+	cout << "Цена тетради(руб) ->";
+	cin >> price_notebook;
+	cout << "Количество тетрадей ->";
+	cin >> cout_notebook;
+	cout << "Цена карандаша(руб)->";
+	cin >> price_pencil;
+	cout << "Количество карандашей ->";
+	cin >> cout_pencil;
+	summa = price_notebook * cout_notebook + price_pencil * cout_pencil;
+	cout << "Стоимость покупки:\n" << summa << endl;
 }
 
