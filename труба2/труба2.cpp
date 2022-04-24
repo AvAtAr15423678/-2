c810f102e139817b270e47e8d325a084188d8b52
Alexandr Trubin <trubin1@yandex.ru>
2022-04-24 17:38:48 +0300

    поменял фсё

--- труба2/труба2.cpp
+++ труба2/труба2.cpp
@@ -1,10 +1,29 @@
-﻿// труба2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <Windows.h>
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a ,b;
+	cin >> a >> b;
+	float c;
+	c = a + b;
+	cout << c << endl;
+	c = a - b;
+	cout << c << endl;
+	c = a * b;
+	cout << c << endl;
+	c = (float)a / b;
+	cout << c << endl;
+	a += b;//a=a+b;
+	a += 1;
+		a++;//инкримент;
+		++a;
+		a--;//декримент;
+		--a;
 }
 
