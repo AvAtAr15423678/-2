fa1cbe62e37e5f27488a33de74d34385651425e8
Alexandr Trubin <trubin1@yandex.ru>
2022-04-24 18:51:04 +0300

    изменение 18:50

--- труба2/труба2.cpp
+++ труба2/труба2.cpp
@@ -9,16 +9,11 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	float width, height;
-	cout << "Вычисление площади треугольника\n"
-		<< "Введите длинну основания треугольника ->";
-	cin >> width;
-	cout << "Введите длинну высоты треугольника ->";
-	cin >> height;
-	cout << "Площадь треугольника:"
-		<< 0.5 * width * height<< "  "
-
-
-		<< width * height / 2 << "кв.см";
+	int start_sec = 75498;
+	//cin>sec;
+	int sec = start_sec % 60;
+	int min = start_sec / 60 % 60;
+	int hour = start_sec / 60 / 60; //start_sec / 3600
+	cout << "h:" << hour << "  m:" << min << "   s:" << sec;
 }
 
